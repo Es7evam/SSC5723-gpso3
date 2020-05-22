94f15950c5a4f783071d9c2d2f5d4e933532398d
Es7evam <estevam.arantes@gmail.com>
2020-05-21 22:20:25 -0300

    Modify simulator to C++ strings standard

--- Modulo_3/Simulador/simulador.cpp
+++ Modulo_3/Simulador/simulador.cpp
@@ -1,76 +1,83 @@
-#include <iostream> 
-#include <list>
-#include <cstring>
+#include "simulador.h"
 
-using namespace std;
+bool leituraPagina(int value){
+	for(auto it : paginas){
+		if(it == value){
+			return true;
+		}
+	}
 
-list<string> paginas;
-list<string>::iterator it;
-int leitura = 0, escrita = 0, op = 0, faltaPagina = 0;
+	return false;
+}
 
-bool leituraPagina(char* value);
-void algoritmoLRU(char* value);
-	
+void algoritmoLRU(int value){
+	paginas.pop_front();
+	paginas.push_back(value);
+}
+
 int main(int argc, char **argv){
 	// definição do arquivo a ser lido
 	FILE *arquivo;
 	char *caminhoArquivo = argv[1];
 	int qtdLinha = 20, qtdValue = 14;
-	char linha[qtdLinha];
+	//char linha[qtdLinha];
 	int tamPagina = 4096, tamMV = 65536, tamMF = 32768;
 	int qtdPagina = tamMF / tamPagina;
 	
 	// abre o arquivo para leitura
-	arquivo = fopen(caminhoArquivo, "r");
 	
+	freopen(caminhoArquivo, "r", stdin);
 	// percorre o arquivo
-	while(fgets(linha, qtdLinha, arquivo) != NULL){
-		op++;
-		// endereco 
-		char value[qtdValue];
-		strncpy(value, linha+5,qtdValue);
-		// numero processo
-		char p[2];
-		strncpy(p, linha,2);
-		
-		// verifica no arquivo um processo novo
-		if (linha[3]  == 'C'){
-			// falta criar o processo
-			
-		// indica a instrução a ser executada pela CPU
-		}else if (linha[3]  == 'P'){
-			// ocorre algo 
-		
-		// indica a instrução de I/O
-		}else if (linha[3]  == 'I'){
-			// ocorre algo
-		
-		// verifica no arquivo se a opcao eh de escrita
-		}else if (linha[3]  == 'W'){
-			
-			if (paginas.size() <= qtdPagina) 
-				paginas.push_back(value);
-			else{
-				algoritmoLRU(value);
-				faltaPagina++;
-			}
-			escrita++;
-			
-		// verifica no arquivo se a opcao eh de leitura
-		}else if (linha[3] == 'R'){
-			// verifica se endereco ja existe
-			if (leituraPagina(value)){
-				cout << "igual"<<endl;
-			}else{
-				paginas.push_back(value);
-			}
-			
-			leitura++;
+	string linha;
+	while(getline(cin, linha)){
+		stringstream ss(linha);
+		string processo, comando;
+		ss >> processo >> comando;
+		if(comando == "C"){
+			int tamanhoProcesso;
+			ss >> tamanhoProcesso;
 		}
+		else{
+			long long operacao;
+			cout << "Linha: " << linha;
+			for(int i=0;i<linha.size();i++){
+				if(linha[i] == '(' || linha[i] == ')'){
+					linha[i] = ' ';
+				} 
+			}
+			ss >> operacao;
+			cout << " : " << operacao << endl;
+			if(comando == "P"){
+
+			}
+			else if(comando == "I"){
+
+			}
+			else if(comando == "W"){
+				if (paginas.size() <= qtdPagina) 
+					paginas.push_back(operacao);
+				else{
+					algoritmoLRU(operacao);
+					faltaPagina++;
+				}
+				escrita++;
+				
+			}else if(comando == "R"){
+			// verifica no arquivo se a opcao eh de leitura
+				// verifica se endereco ja existe
+				if (leituraPagina(operacao)){
+					cout << "igual"<<endl;
+				}else{
+					paginas.push_back(operacao);
+				}
+				
+				leitura++;
+			}
+		}
+	
 	}
 	
 	// fecha o arquivo 
-	fclose(arquivo);
 	
 	// mostra no terminal algumas informacoes referentes ao simulador
 	cout << "\nSimulador Memoria Virtual" << endl;
@@ -80,18 +87,4 @@ int main(int argc, char **argv){
 	cout << "Quantidade de falta de pagina: " << faltaPagina << endl;
 	
 	return 0;
-}
-
-bool leituraPagina(char* value){
-	for(it = paginas.begin(); it != paginas.end(); it++){
-		if(*it == value)
-			return true;
-	}
-
-	return false;
-}
-
-void algoritmoLRU(char* value){
-	paginas.pop_front();
-	paginas.push_back(value);
 }
